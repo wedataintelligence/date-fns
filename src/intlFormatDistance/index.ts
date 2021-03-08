e8170718e28b43a55dc97ba656fd9ef256fcd8b8
Tan75 <ttobin@protonmail.ch>
2021-03-08 15:52:22 +0000

    Update src/intlFormatDistance/index.ts
    Co-authored-by: Sasha Koss <koss@nocorp.me>

--- src/intlFormatDistance/index.ts
+++ src/intlFormatDistance/index.ts
@@ -179,7 +179,7 @@ const secondsInQuarter = secondsInMonth * 3
  * const result = intlFormatDistance(
  *   new Date(1986, 3, 4, 11, 30, 0),
  *   new Date(1986, 3, 4, 10, 30, 0),
- *   { unit: 'minute', locales: 'es' });
+ *   { unit: 'minute', locale: 'es' });
  * // => dentro de 60 minutos
  *
  * @example
